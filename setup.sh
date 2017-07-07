d7a9a6514a80f68d0d645d7fedc20e16548fba8c
Justin Leber <justin@openvpn.net>
2017-07-07 10:02:00 -0700

    added setup.sh script to initialize vagrant

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Variables can be changed to use different OS/releases
+OS=ubuntu
+RELEASE=xenial64
+
+vagrant init $OS/$RELEASE
+
+# Clear default Vagrantfile
+echo "" > Vagrantfile
+cat > Vagrantfile << 'EOF'
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.network "private_network", ip: "192.168.33.10"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.memory = "1024"
+  end
+
+  config.vm.provision "shell", inline: <<-SHELL
+    apt-get update
+    apt-get install -y python 
+  SHELL
+  
+config.vm.provision "ansible" do |ansible|
+  ansible.playbook = "site.yml"
+end
+end
+EOF
